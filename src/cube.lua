08874aa2ec0a8454e0ef036fe72d855c20886970
kader <kaderarnold@icloud.com>
2023-07-27 21:32:54 -0700

    init

---
+++ src/cube.lua
@@ -0,0 +1,58 @@
+cube = {}
+cube.mt = {}
+
+setmetatable(cube, cube.mt)
+function cube.mt:__call ()
+   local cube = {} 
+   function cube:load (size)
+    if size then
+        cube.canvas = love.graphics.newCanvas(size, size) 
+    else
+        cube.canvas = love.graphics.newCanvas(32, 32) 
+    end
+   cube.points = {
+        {
+					x = cube.canvas:getWidth()/2,
+					y = 0 
+        },
+        {
+					x = cube.canvas:getWidth(),
+					y = cube.canvas:getHeight()/4
+				},
+        {
+					x = cube.canvas:getWidth()/2,
+					y = cube.canvas:getHeight()/4
+        },
+        {
+					x = 0,
+					y = cube.canvas:getHeight()/4
+        },
+				{
+					x = cube.canvas:getHeight()/2,
+					y = 0
+				},
+				{
+					x = cube.canvas:getWidth()/2,
+					y = cube.canvas:getHeight()/4
+				}
+   }
+   end
+   function cube:render ()
+    love.graphics.setCanvas(cube.canvas)
+		for i=1, #cube.points - 1 do
+			love.graphics.line(cube.points[i].x, cube.points[i].y, cube.points[i+1].x, cube.points[i+1].y)
+		end
+    love.graphics.setCanvas()
+   end
+
+   function cube:update (dt)
+			cube:render()
+   end
+
+   function cube:draw()
+			love.graphics.draw(cube.canvas)
+   end
+   return cube
+end
+
+return cube
---
+++ src/main.lua
@@ -0,0 +1,17 @@
+Cube = require "cube"
+function love.load ()
+    cube = Cube()
+    cube:load (32)
+    
+end
+
+function love.update (dt)
+   cube:update(dt)
+	 if love.keyboard.isDown('q') then
+		 love.event.quit ('1')
+	 end
+end
+
+function love.draw ()
+   cube:draw()
+ end
